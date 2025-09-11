2e7783a9d5d718da3ee8b196840bc7ef33cb4970
Phsithzz <phoosith2548@gmail.com>
2025-09-11 11:41:24 +0700

    Update

--- backend/Controllers/memberController.js
+++ backend/Controllers/memberController.js
@@ -22,7 +22,7 @@ export const postMember = async (req, res) => {
     const pwd = req.body.password
     const saltround = 11
     const pwdHash = await bcrypt.hash(pwd,saltround)
-    
+
     const result = await database.query({
       text: `INSERT INTO "members" ("memEmail","memName","memHash") VALUES($1,$2,$3)`,
       values: [
@@ -37,6 +37,51 @@ export const postMember = async (req, res) => {
     bodyData.createDate = datetime;
     bodyData.message = "OK";
     res.json(bodyData);
+  } catch (err) {
+    console.log(err);
+    return res.json({
+      message: "Internal Server Error",
+      error: err.message,
+    });
+  }
+};
+
+export const loginMember = async (req, res) => {
+  console.log("POST /login is requested.");
+  try {
+    const bodyData = req.body;
+    if (!bodyData.loginName || !bodyData.password) {
+      return res.json({
+        message: " Error Email and Password is required",
+      });
+    }
+    const result = await database.query({
+      text: `SELECT * FROM members WHERE "memEmail"=$1`,
+      values: [req.body.loginName],
+    });
+    if (result.rowCount == 0) {
+
+      return res.json({
+        message: `ERROR Login fail `,
+        login:false
+      });
+    }
+
+    const loginOk = await bcrypt.compare(req.body.password,result.rows[0].memHash)
+
+    if(loginOk){
+        res.json({
+            message:`Login Success `,login:true
+
+        })
+    }
+    else{
+        res.json({
+            message:"Login Fail",login:false
+        })
+    }
+
+
   } catch (err) {
     console.log(err);
     return res.json({
--- backend/Routes/memberRoute.js
+++ backend/Routes/memberRoute.js
@@ -5,6 +5,6 @@ import * as memberController from "../Controllers/memberController.js"
 const router = express.Router()
 
 router.post("/members",memberController.postMember)
-
+router.post("/login",memberController.loginMember)
 
 export default router 
